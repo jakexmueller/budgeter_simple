3ecfa2596dd31516ec22dae7cfaec5ef42dede04
Jacob <jakexmueller@gmail.com>
2019-01-26 13:40:23 -0600

    added style sheet

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ function printSalary(salary){
 }
 
 function runBudgeter(){
+    var budget = [];
     var salary = prompt('Please enter your annual salary', '40000');
     var rent = prompt('Please enter your monthly rent', '650')
+    budget.push(rent);
     var transportationBudget = prompt('Please enter your monthly transportation budget (vehicle payments, insurance, gas, etc.)', '250')
+    budget.push(transportationBudget);
     var foodBudget = prompt('Please enter your monthly food budget', '300')
-    document.write('Salary: $' + salary);
-    document.write('Rent: $' + rent);
-    document.write('Transportation: $' + transportationBudget);
-    document.write('Food Budget: $' + foodBudget);
+    budget.push(foodBudget);
+    document.getElementById('demo').innerHTML=salary;
+    document.getElementById('demo').innerHTML=budget;
 }
 
 function printName(){
