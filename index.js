bc160c9ad44a7c6d412faffd4ff7cdd058f54672
Jacob <jakexmueller@gmail.com>
2019-01-26 12:56:41 -0600

    added rent, transportation, and food

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-"use strict"
+'use strict'
 
 function printSalary(salary){
-    document.getElementById("demo").innerHTML =
+    document.getElementById('demo').innerHTML =
     salary;
 }
 
-function getIncome(){
-    var salary = prompt("Please enter your annual salary", "40000");
-    document.write('$' + salary);
+function runBudgeter(){
+    var salary = prompt('Please enter your annual salary', '40000');
+    var rent = prompt('Please enter your monthly rent', '650')
+    var transportationBudget = prompt('Please enter your monthly transportation budget (vehicle payments, insurance, gas, etc.)', '250')
+    var foodBudget = prompt('Please enter your monthly food budget', '300')
+    document.write('Salary: $' + salary);
+    document.write('Rent: $' + rent);
+    document.write('Transportation: $' + transportationBudget);
+    document.write('Food Budget: $' + foodBudget);
 }
 
 function printName(){
